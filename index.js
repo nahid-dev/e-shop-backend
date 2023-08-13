2d207e96caf345379be048931a358ca42693aec1
nahid-dev <nahidv232@gmail.com>
2023-08-13 12:56:27 +0600

    update two

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // ALL COLLECTION HERE =========>
     const productCollection = client.db("eShopServer").collection("products");
